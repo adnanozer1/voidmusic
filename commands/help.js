f958cc2182e1800927cfbc8116703f4680f7b4c9
adnanozer1 <101367438+adnanozer1@users.noreply.github.com>
2022-04-22 01:31:35 +0300

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,30 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports.config = { 
+     name: 'müzikyardım',
+     aliases: ['müzik']
+ }
+ 
+ module.exports.sex = async(client, message, args, config) => {
+
+let prefix = config.BotPrefixes[Math.floor(Math.random() * config.BotPrefixes.length)];
+
+message.channel.send(new MessageEmbed().setAuthor(`${message.client.user.username}`, message.client.user.avatarURL()).setColor('RANDOM')
+.addField('Müzik Komutları',`
+
+**${prefix}play <müzikismi>**: İstenilen şarkıyı çalar.
+**${prefix}pause**: Şarkıyı durdurur.
+**${prefix}resume**: Durdurulan şarkıyı devam ettirir.
+**${prefix}loop**: Şarkıyı tekrardan oynatır.
+**${prefix}skip**: Varsa listedeki diğer şarkılara geçer.
+**${prefix}stop**: Şarkıyı durdurur. 
+**${prefix}np**: o an çalan şarkıyı gösterir.
+**${prefix}list**: Şarkıları gösterir.
+**${prefix}mix**: Şarkıları karıştırır.
+**${prefix}ping**: Botun gecikme süresini gösterir.
+**Sunucunuza eklemek isterseniz:**[davet linki](https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8)
+`)
+.setFooter(`${message.author.tag} Tarafından İstendi.`, message.author.avatarURL)
+.setThumbnail("https://cdn.discordapp.com/attachments/955817573210030080/959510885221691492/spinning-music.gif"))
+
+};
